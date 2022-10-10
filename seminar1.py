1ff68e18aed35ed7afcf7eae69fcdea3202e7a5c
Svetlana <tina31@rambler.ru>
2022-10-10 11:43:06 +0300

    S

---
+++ seminar1.py
@@ -0,0 +1,68 @@
+#print ('hello world')
+#a = 2
+#print (a)
+
+
+
+#Задача 1
+#N1 = int(input('Введите первое число: '))
+#N2 = int(input('Введите второе число: '))
+
+#if N2 == N1 * N1:
+#    print("Да")
+#elif N1 == N2 * N2:
+ #   print("Да")
+#else:
+ #   print("Нет")
+
+# Задача 2 найти макс.число
+# max = int(input(f'Введите 1 число: '))
+
+# for i in range(4):
+#     i += 1
+#     N = int(input(f'Введите {i+1} число: '))
+#     if N > max:
+#         max = N
+# print(max)
+
+#Задача 3 
+
+
+#a = int(input('Введите число: '))
+#a=abs(a)
+#for i in range(-a, a+1):
+   # print (i, end= '; ')
+
+# Задача 4
+# a =float (input("в"))
+# b =  (a * 10 % 10)
+
+# if a%1 == 0 :
+#     b = None
+# print (b)
+
+
+#Задача 4
+
+# N = float(input('Введите число N: '))
+# if N % 1 == 0:
+#     print("нет")
+#     quit()
+
+# N *= 10
+# N = int(N % 10)
+
+# print(N)
+
+
+
+# Задача 5
+    #Напишите программу, которая принимает на вход число и проверяет, кратно ли оно 5 и 10 или 15, но не 30
+
+#Задача 5
+#N = int(input('Введите число N: '))
+
+#if((N % 5 == 0 and N % 10 == 0) or (N % 15 == 0)) and (N % 30 != 0):
+ #   print("Да")
+#else:
+ #   print("нет")
